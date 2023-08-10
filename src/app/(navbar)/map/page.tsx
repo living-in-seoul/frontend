136c19ca1667bac03b2cd889f399f82cddb99556
nayoung3669 <skdud3669@gmail.com>
2023-08-10 21:06:46 +0900

    [feat] folder structure for modal

--- src/app/(navbar)/map/page.tsx
+++ src/app/(navbar)/map/page.tsx
@@ -1,5 +1,3 @@
-import Map from '@/components/map/Map';
-import MapBottomSheet from '@/components/map/MapBottomSheet';
 import dynamic from 'next/dynamic';
 const DynamicMap = dynamic(() => import('../../../components/map/Map'), {
   ssr: false,
