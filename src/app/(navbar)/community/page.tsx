af13ac83467bef2d2398955859e4d0f009f74bd8
nayoung3669 <skdud3669@gmail.com>
2023-08-10 09:40:38 +0900

    [feat] map bottom sheet & constants

--- src/app/(navbar)/community/page.tsx
+++ src/app/(navbar)/community/page.tsx
@@ -1,4 +1,351 @@
 const CommunityPage = () => {
-  return <div>CommunityPage</div>;
+  return (
+    <>
+      <div className="w-96 h-96 relative bg-white">
+        <div className="w-96 h-32 left-0 top-0 absolute bg-zinc-300" />
+        <div className="w-80 h-3.5 left-[42.24px] top-[15.60px] absolute">
+          <div className="w-6 h-3.5 left-[297.81px] top-0 absolute"></div>
+        </div>
+        <div className="w-96 h-20 left-0 top-[1554.84px] absolute">
+          <div className="w-96 h-20 left-0 top-0 absolute bg-white border-t border-zinc-400" />
+        </div>
+        <div className="w-96 h-11 left-[16px] top-[55px] absolute">
+          <div className="w-96 h-11 left-0 top-0 absolute bg-white rounded-3xl" />
+          <div className="left-[21px] top-[17px] absolute text-zinc-500 text-sm font-normal leading-3">
+            #해시태그로 검색해 보세요.
+          </div>
+        </div>
+        <div className="left-[16px] top-[140.85px] absolute text-black text-lg font-semibold leading-loose">
+          실시간 인기글
+        </div>
+        <div className="left-[16px] top-[629.01px] absolute text-black text-lg font-semibold leading-loose">
+          이웃이 남긴 후기를 살펴보세요
+        </div>
+        <div className="left-[16px] top-[1085.13px] absolute text-black text-lg font-semibold leading-loose">
+          동향 사람들과 소통해 보세요
+        </div>
+        <div className="w-14 h-14 left-[16px] top-[236.52px] absolute">
+          <div className="w-14 h-14 left-0 top-0 absolute bg-zinc-400 rounded-full" />
+          <div className="w-11 h-11 left-[6.29px] top-[6.29px] absolute" />
+        </div>
+        <div className="w-14 h-14 left-[16px] top-[348.24px] absolute">
+          <div className="w-14 h-14 left-0 top-0 absolute bg-zinc-400 rounded-full" />
+          <div className="w-11 h-11 left-[6.29px] top-[6.29px] absolute" />
+        </div>
+        <div className="w-14 h-14 left-[16px] top-[460.47px] absolute">
+          <div className="w-14 h-14 left-0 top-0 absolute bg-zinc-400 rounded-full" />
+          <div className="w-11 h-11 left-[6.29px] top-[6.29px] absolute" />
+        </div>
+        <div className="w-56 h-14 left-[83.12px] top-[235.52px] absolute">
+          <div className="left-[2.88px] top-[24.80px] absolute text-black text-xs font-semibold leading-3">
+            닉네임
+          </div>
+          <div className="left-[2.88px] top-[42.60px] absolute">
+            <span className="text-black text-xs font-normal underline leading-3">
+              #중국집
+            </span>
+            <span className="text-black text-xs font-normal leading-3">
+              {' '}
+              추천 부탁드립니다ㅠㅠ 배달 시켜 먹으려고 해용
+            </span>
+          </div>
+          <div className="w-10 h-4 px-2 py-1 left-0 top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-zinc-600 text-xs font-normal leading-3">
+              #맛집
+            </div>
+          </div>
+        </div>
+        <div className="w-56 h-14 left-[83.12px] top-[347.24px] absolute">
+          <div className="left-[2.88px] top-[24.80px] absolute text-black text-xs font-semibold leading-3">
+            닉네임
+          </div>
+          <div className="left-[2.88px] top-[42.60px] absolute">
+            <span className="text-black text-xs font-normal leading-3">
+              근처에 시원한 생맥주랑 구운 치킨 파는{' '}
+            </span>
+            <span className="text-black text-xs font-normal underline leading-3">
+              #치킨집
+            </span>
+            <span className="text-black text-xs font-normal leading-3">
+              {' '}
+              없나요?
+            </span>
+          </div>
+          <div className="w-10 h-4 px-2 py-1 left-0 top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-zinc-600 text-xs font-normal leading-3">
+              #맛집
+            </div>
+          </div>
+        </div>
+        <div className="w-64 h-14 left-[83.12px] top-[459.47px] absolute">
+          <div className="left-[2.88px] top-[24.80px] absolute text-black text-xs font-semibold leading-3">
+            닉네임
+          </div>
+          <div className="left-[2.88px] top-[42.60px] absolute">
+            <span className="text-black text-xs font-normal leading-3">
+              배달 시켜 먹으려고 하는데{' '}
+            </span>
+            <span className="text-black text-xs font-normal underline leading-3">
+              #마라탕
+            </span>
+            <span className="text-black text-xs font-normal leading-3">
+              {' '}
+              위생적인 곳으로 추천해 주세요!
+            </span>
+          </div>
+          <div className="w-10 h-4 px-2 py-1 left-0 top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-zinc-600 text-xs font-normal leading-3">
+              #맛집
+            </div>
+          </div>
+        </div>
+        <div className="w-96 h-px left-0 top-[330.74px] absolute border border-zinc-400"></div>
+        <div className="w-96 h-px left-0 top-[442.47px] absolute border border-zinc-400"></div>
+        <div className="h-3.5 left-[86px] top-[299.24px] absolute">
+          <div className="w-3.5 h-3.5 left-0 top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[45px] top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[90px] top-0 absolute flex-col justify-start items-start inline-flex" />
+        </div>
+        <div className="h-3.5 left-[86px] top-[410.97px] absolute">
+          <div className="w-3.5 h-3.5 left-0 top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[45px] top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[90px] top-0 absolute flex-col justify-start items-start inline-flex" />
+        </div>
+        <div className="h-3.5 left-[86px] top-[523.20px] absolute">
+          <div className="w-3.5 h-3.5 left-0 top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[45px] top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[90px] top-0 absolute flex-col justify-start items-start inline-flex" />
+        </div>
+        <div className="w-96 h-1 left-0 top-[604.70px] absolute bg-zinc-300" />
+        <div className="w-96 h-1 left-0 top-[1061.41px] absolute bg-zinc-300" />
+        <div className="left-[16px] top-[187.81px] absolute">
+          <div className="w-20 h-7 px-3.5 left-0 top-[0.20px] absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-black text-xs font-normal leading-loose">
+              #생활정보
+            </div>
+          </div>
+          <div className="w-14 h-7 px-3.5 left-[89px] top-[0.20px] absolute bg-neutral-700 rounded-3xl justify-center items-center gap-2.5 inline-flex">
+            <div className="text-white text-xs font-normal leading-loose">
+              #맛집
+            </div>
+          </div>
+          <div className="w-14 h-7 px-3.5 left-[157px] top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-black text-xs font-normal leading-loose">
+              #꿀팁
+            </div>
+          </div>
+          <div className="w-24 h-7 px-3.5 left-[225px] top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-black text-xs font-normal leading-loose">
+              #도와주세요
+            </div>
+          </div>
+          <div className="w-24 h-7 px-3.5 left-[325px] top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-black text-xs font-normal leading-loose">
+              #빌려주세요
+            </div>
+          </div>
+        </div>
+        <div className="left-[16px] top-[675.79px] absolute">
+          <div className="w-20 h-7 px-3.5 left-0 top-[0.20px] absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-black text-xs font-normal leading-loose">
+              #생활정보
+            </div>
+          </div>
+          <div className="w-14 h-7 px-3.5 left-[89px] top-[0.20px] absolute bg-neutral-700 rounded-3xl justify-center items-center gap-2.5 inline-flex">
+            <div className="text-white text-xs font-normal leading-loose">
+              #맛집
+            </div>
+          </div>
+          <div className="w-14 h-7 px-3.5 left-[157px] top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-black text-xs font-normal leading-loose">
+              #꿀팁
+            </div>
+          </div>
+          <div className="w-24 h-7 px-3.5 left-[225px] top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-black text-xs font-normal leading-loose">
+              #도와주세요
+            </div>
+          </div>
+          <div className="w-24 h-7 px-3.5 left-[325px] top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-black text-xs font-normal leading-loose">
+              #빌려주세요
+            </div>
+          </div>
+        </div>
+        <div className="px-3.5 left-[16px] top-[1132.11px] absolute bg-neutral-700 rounded-3xl justify-center items-center gap-2.5 inline-flex">
+          <div className="text-white text-xs font-normal leading-loose">
+            #부산
+          </div>
+        </div>
+        <div className="px-3.5 left-[84px] top-[1132.11px] absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+          <div className="text-black text-xs font-normal leading-loose">
+            #경남
+          </div>
+        </div>
+        <div className="px-3.5 left-[152px] top-[1132.11px] absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+          <div className="text-black text-xs font-normal leading-loose">
+            #경북
+          </div>
+        </div>
+        <div className="px-3.5 left-[220px] top-[1132.11px] absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+          <div className="text-black text-xs font-normal leading-loose">
+            #울산
+          </div>
+        </div>
+        <div className="px-3.5 left-[288px] top-[1132.11px] absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+          <div className="text-black text-xs font-normal leading-loose">
+            #대구
+          </div>
+        </div>
+        <div className="px-3.5 left-[356px] top-[1132.11px] absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+          <div className="text-black text-xs font-normal leading-loose">
+            #대전
+          </div>
+        </div>
+        <div className="w-96 h-80 left-[16px] top-[717.72px] absolute">
+          <div className="w-20 h-20 left-[278px] top-0 absolute bg-white rounded-2xl shadow" />
+          <div className="w-64 h-14 left-0 top-[14px] absolute">
+            <div className="w-16 h-2.5 left-0 top-0 absolute">
+              <div className="left-0 top-0 absolute text-black text-xs font-semibold leading-3">
+                닉네임
+              </div>
+              <div className="left-[41px] top-0 absolute text-zinc-600 text-xs font-normal leading-3">
+                동네명
+              </div>
+            </div>
+            <div className="w-64 h-7 left-0 top-[25px] absolute">
+              <div className="left-0 top-0 absolute text-black text-sm font-semibold leading-3">
+                혼밥하시는 분들에게 굉장히 좋을 식당 후기
+              </div>
+              <div className="left-0 top-[20px] absolute text-zinc-600 text-xs font-normal leading-3">
+                서울에서 올라온 지 얼마 안돼 친구가 없어서 밥을 먹…
+              </div>
+            </div>
+          </div>
+          <div className="w-20 h-20 left-[278px] top-[98px] absolute bg-white rounded-2xl shadow" />
+          <div className="w-20 h-20 left-[278px] top-[196px] absolute bg-white rounded-2xl shadow" />
+          <div className="w-64 h-14 left-0 top-[112px] absolute">
+            <div className="w-16 h-2.5 left-0 top-0 absolute">
+              <div className="left-0 top-0 absolute text-black text-xs font-semibold leading-3">
+                닉네임
+              </div>
+              <div className="left-[41px] top-0 absolute text-zinc-600 text-xs font-normal leading-3">
+                동네명
+              </div>
+            </div>
+            <div className="w-64 h-7 left-0 top-[25px] absolute">
+              <div className="left-0 top-0 absolute text-black text-sm font-semibold leading-3">
+                서울에 온 뒤로 역대급 식당에서 밥 먹었습니다
+              </div>
+              <div className="left-0 top-[20px] absolute text-zinc-600 text-xs font-normal leading-3">
+                안녕하세요 서울에 올라온 뒤로 맛있는 밥을 먹기 힘들…
+              </div>
+            </div>
+          </div>
+          <div className="w-96 h-32 left-0 top-[210px] absolute">
+            <div className="w-16 h-2.5 left-0 top-0 absolute">
+              <div className="left-0 top-0 absolute text-black text-xs font-semibold leading-3">
+                닉네임
+              </div>
+              <div className="left-[41px] top-0 absolute text-zinc-600 text-xs font-normal leading-3">
+                동네명
+              </div>
+            </div>
+            <div className="w-64 h-7 left-0 top-[25px] absolute">
+              <div className="left-0 top-0 absolute text-black text-sm font-semibold leading-3">
+                맛은 있었지만 친절함이 부족했던 식당
+              </div>
+              <div className="left-0 top-[20px] absolute text-zinc-600 text-xs font-normal leading-3">
+                혼밥을 하기 위해서 여러 군데 둘러보다 추천을 받고 들…
+              </div>
+            </div>
+            <div className="w-12 h-7 left-[308px] top-[94px] absolute">
+              <div className="left-0 top-0 absolute text-black text-xs font-bold leading-loose">
+                더보기
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="left-[324px] top-[564.01px] absolute">
+          <div className="left-0 top-0 absolute text-black text-xs font-bold leading-loose">
+            더보기
+          </div>
+        </div>
+        <div className="w-14 h-14 left-[15.50px] top-[1186.50px] absolute">
+          <div className="w-14 h-14 left-0 top-0 absolute bg-zinc-400 rounded-full" />
+          <div className="w-11 h-11 left-[6.29px] top-[6.29px] absolute" />
+        </div>
+        <div className="w-14 h-14 left-[15.50px] top-[1298.23px] absolute">
+          <div className="w-14 h-14 left-0 top-0 absolute bg-zinc-400 rounded-full" />
+          <div className="w-11 h-11 left-[6.29px] top-[6.29px] absolute" />
+        </div>
+        <div className="w-14 h-14 left-[15.50px] top-[1410.45px] absolute">
+          <div className="w-14 h-14 left-0 top-0 absolute bg-zinc-400 rounded-full" />
+          <div className="w-11 h-11 left-[6.29px] top-[6.29px] absolute" />
+        </div>
+        <div className="left-[85.50px] top-[1210.30px] absolute text-black text-xs font-semibold leading-3">
+          닉네임
+        </div>
+        <div className="left-[85.50px] top-[1228.10px] absolute text-black text-xs font-normal leading-3">
+          반가워용ㅎㅎ 저번 달부터 서울살이 시작했습니다ㅠㅠㅠ
+        </div>
+        <div className="left-[85.50px] top-[1322.03px] absolute text-black text-xs font-semibold leading-3">
+          닉네임
+        </div>
+        <div className="left-[85.50px] top-[1339.83px] absolute text-black text-xs font-normal leading-3">
+          돼지국밥이 너무 먹고 싶어요ㅠㅠ 서울에서는 그 맛이 안 나네요요
+        </div>
+        <div className="px-2 py-1 left-[82.62px] top-[1297.23px] absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+          <div className="text-zinc-600 text-xs font-normal leading-3">
+            #부산
+          </div>
+        </div>
+        <div className="px-2 py-1 left-[82.62px] top-[1186.11px] absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+          <div className="text-zinc-600 text-xs font-normal leading-3">
+            #부산
+          </div>
+        </div>
+        <div className="w-20 h-14 left-[82.62px] top-[1409.45px] absolute">
+          <div className="left-[2.88px] top-[24.80px] absolute text-black text-xs font-semibold leading-3">
+            닉네임
+          </div>
+          <div className="left-[2.88px] top-[42.60px] absolute text-black text-xs font-normal leading-3">
+            바다 보고싶다......
+          </div>
+          <div className="w-10 h-4 px-2 py-1 left-0 top-0 absolute rounded-3xl border border-zinc-400 justify-center items-center gap-2.5 inline-flex">
+            <div className="text-zinc-600 text-xs font-normal leading-3">
+              #부산
+            </div>
+          </div>
+        </div>
+        <div className="w-96 h-px left-[-0.50px] top-[1280.73px] absolute border border-zinc-400"></div>
+        <div className="w-96 h-px left-[-0.50px] top-[1392.45px] absolute border border-zinc-400"></div>
+        <div className="h-3.5 left-[85.50px] top-[1249.23px] absolute">
+          <div className="w-3.5 h-3.5 left-0 top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[45px] top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[90px] top-0 absolute flex-col justify-start items-start inline-flex" />
+        </div>
+        <div className="h-3.5 left-[85.50px] top-[1360.95px] absolute">
+          <div className="w-3.5 h-3.5 left-0 top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[45px] top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[90px] top-0 absolute flex-col justify-start items-start inline-flex" />
+        </div>
+        <div className="h-3.5 left-[85.50px] top-[1473.18px] absolute">
+          <div className="w-3.5 h-3.5 left-0 top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[45px] top-0 absolute flex-col justify-start items-start inline-flex" />
+          <div className="w-3.5 h-3.5 left-[90px] top-0 absolute flex-col justify-start items-start inline-flex" />
+        </div>
+        <div className="left-[323.50px] top-[1513.99px] absolute">
+          <div className="left-0 top-0 absolute text-black text-xs font-bold leading-loose">
+            더보기
+          </div>
+        </div>
+        <div className="w-12 h-12 left-[326px] top-[1465.99px] absolute">
+          <div className="w-12 h-12 left-0 top-0 absolute bg-neutral-700 rounded-full" />
+          <div className="w-6 h-6 left-[12px] top-[12px] absolute" />
+        </div>
+      </div>
+    </>
+  );
 };
 export default CommunityPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import Map from "@/components/map/Map";
-import dynamic from "next/dynamic";
-
-const DynamicMap = dynamic(() => import("../components/map/Map"), {
-  ssr: false,
-});
 export default function Home() {
-  return (
-    <>
-      <DynamicMap />
-    </>
-  );
+  return <></>;
 }
