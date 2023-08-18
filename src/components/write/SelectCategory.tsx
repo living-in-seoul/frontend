14249b07bb44369c7a4583ce787b192efa2d23ee
nayoung3669 <skdud3669@gmail.com>
2023-08-19 02:06:04 +0900

    [feat] write page POST

---
+++ src/components/write/SelectCategory.tsx
@@ -0,0 +1,46 @@
+import { checked } from '@/utils/Icon';
+import { BoardOptions } from '@/utils/constants/board';
+import Icons from '../common/Icons';
+
+interface SelectCategoryProp {
+  selectedOption: string | null;
+  onSelectOptionHandler: (data: number) => void;
+}
+
+const SelectCategory = ({
+  selectedOption,
+  onSelectOptionHandler,
+}: SelectCategoryProp) => {
+  return (
+    <div className="flex flex-col gap-1.5 px-4 h-full">
+      <span className="w-full text-[1.1rem] font-semibold mb-2.5">
+        주제 선택
+      </span>
+      <div className="flex flex-col justify-between  h-full">
+        {BoardOptions.map((option, idx) => {
+          const selected = selectedOption === option;
+          return (
+            <div
+              className="flex justify-around items-center w-full h-full  "
+              key={idx}
+            >
+              <span
+                className={`text-xs w-[95%] h-full my-0.5 ${
+                  !selected && selectedOption && `text-zinc-400`
+                }`}
+                onClick={() => onSelectOptionHandler(idx)}
+              >
+                {option}
+              </span>
+              <span className="w-[5%] h-full text-zinc-600">
+                {selected && <Icons path={checked} />}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default SelectCategory;
--- src/components/write/UploadImageModal.tsx
+++ src/components/write/UploadImageModal.tsx
@@ -8,19 +8,16 @@ interface UploadImageModalProps {
 }
 
 const UploadImageModal = ({ onClose }: UploadImageModalProps) => {
-  const [postImg, setPostImg] = useState<FileList | null>(null);
   const setOpenImagePortal = useSetRecoilState(ImagePortalState);
   const [imageState, setImageState] = useRecoilState(ImageState);
 
   const onChangePostImg = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
-      setPostImg(e.target.files);
+      const filesArray = Array.from(e.target.files); // FileList를 File[]로 변환
+      setImageState(filesArray);
       setOpenImagePortal(false);
     }
-  };
-
-  const onAddPostImg = () => {
-    setImageState(postImg);
+    console.log(imageState);
   };
 
   return (
@@ -28,7 +25,6 @@ const UploadImageModal = ({ onClose }: UploadImageModalProps) => {
       <div className=" items-center flex justify-center w-full h-10 text-center text-sm">
         사진 업로드
       </div>
-
       <div className="relative w-full h-16">
         <label className="absolute w-full h-full flex items-center justify-center cursor-pointer">
           갤러리에서 가져오기
@@ -36,11 +32,11 @@ const UploadImageModal = ({ onClose }: UploadImageModalProps) => {
             className="opacity-0 absolute w-full h-full"
             name="postImg"
             type="file"
+            multiple
             onChange={(e) => onChangePostImg(e)}
           />
         </label>
       </div>
-
       <div className="w-full flex justify-center items-center">
         <Button
           title="닫기"
