dba4e90c1ceb6e0140e38d15b871423f2efe92bc
nayoung3669 <skdud3669@gmail.com>
2023-08-22 10:05:01 +0900

    [feat] community map states

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,13 +1,13 @@
-import MapHeader from '@/components/map/MapHeader';
-import Map from '../../components/map/Map';
+import MapHeader from '@/components/map/header/MapHeader';
+import MapOption from '@/components/map/MapOption';
 
-const MapPage = () => {
+export default function MapPage() {
   return (
     <section className=" w-full h-full relative">
       <MapHeader />
-      <Map />
+      {/* 삼항연산자로 넣어라 */}
+      <MapOption />
       <div id="portal" />
     </section>
   );
-};
-export default MapPage;
+}
---
+++ src/components/common/Toggle.tsx
@@ -0,0 +1,39 @@
+interface ToggleProps {
+  options: {
+    key: MapToggle;
+    name: string;
+  }[];
+  active: string;
+  toggleHandler: (data: MapToggle) => void;
+}
+
+const Toggle = ({ options, active, toggleHandler }: ToggleProps) => {
+  const activeIndex = options.findIndex((option) => option.key === active);
+  const toggleWidth = 100 / options.length;
+
+  return (
+    <div className="relative flex justify-between items-center px-2 w-[170px] h-8 bg-neutral-500 rounded-2xl text-[0.72rem] ">
+      <div
+        className="absolute top-0 left-0 h-[78%] w-full mx-2 my-1 flex justify-center items-center bg-white transition-transform duration-500 ease-in-out rounded-2xl"
+        style={{
+          width: `${toggleWidth - 4}%`,
+          transform: `translateX(${toggleWidth * activeIndex * 2}%)`,
+        }}
+      ></div>
+      {options.map((option, _) => {
+        const isActive = option.key === active;
+        return (
+          <div
+            key={option.key}
+            className="flex items-center justify-center w-full  rounded-xl z-10 relative"
+            onClick={() => toggleHandler(option.key)}
+          >
+            {option.name + ' 맵'}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Toggle;
