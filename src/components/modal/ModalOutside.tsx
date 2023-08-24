a8938b1836ad2a5dfdbc589bfd4f34faa4ec6f91
nayoung3669 <skdud3669@gmail.com>
2023-08-24 10:12:35 +0900

    [feat] image preview, image upload modal

--- src/components/modal/ModalOutside.tsx
+++ src/components/modal/ModalOutside.tsx
@@ -9,7 +9,7 @@ interface ModalOutsideProps {
 const ModalOutside = ({ children, onClose, className }: ModalOutsideProps) => {
   return (
     <section
-      className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full z-50 bg-neutral-900/10"
+      className="fixed top-0 left-0 flex flex-col justify-center items-center w-full h-full z-50 bg-black opacity-40"
       onClick={(event) => {
         if (event.target === event.currentTarget) {
           onClose();
