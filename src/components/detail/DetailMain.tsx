82abcba747416998ffa5911d886b10f1f92f9699
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-08-24 18:16:30 +0900

    [hotfix] detailpage upload

--- src/components/detail/DetailMain.tsx
+++ src/components/detail/DetailMain.tsx
@@ -1,5 +1,6 @@
 import DetailMainProfile from './DetailMainProfile';
 import DetailMainContents from './DetailMainContents';
+import DetailMainTags from './DetailMainTags';
 
 export interface DetailMainProps extends Post {
   nickname: string;
@@ -8,9 +9,10 @@ export interface DetailMainProps extends Post {
 
 const DetailMain = ({ data }: { data: DetailMainProps }) => {
   return (
-    <section>
+    <section className="flex flex-col gap-6 py-6 border-b-2">
       <DetailMainProfile data={data} />
       <DetailMainContents data={data} />
+      <DetailMainTags data={data} />
     </section>
   );
 };
