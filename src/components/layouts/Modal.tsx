09278b7bdf15655a1de1fccf9ca6f2e8d908dea0
최은석 <trgf456@naver.com>
2023-09-07 01:44:10 +0900

    [fix] home place add

--- src/components/layouts/Modal.tsx
+++ src/components/layouts/Modal.tsx
@@ -1,18 +1,23 @@
 'use client';
-
+import useOutsideClick from '@/hooks/useOutsideClick';
 import { useEffect, useRef } from 'react';
 
 interface Props {
   children: React.ReactNode;
+  onClose: () => void;
 }
 
-const Modal = ({ children }: Props) => {
-  const ref = useRef<HTMLElement>(null);
+const Modal = ({ children, onClose }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const scrollY = useRef(0);
+  useOutsideClick(ref, () => {
+    onClose();
+  });
   useEffect(() => {
     const modalElement = ref.current;
     if (modalElement) {
       modalElement.classList.add('modal-entering');
-
+      scrollY.current = window.scrollY;
       setTimeout(() => {
         modalElement.classList.remove('modal-entering');
         modalElement.classList.add('modal-opened');
@@ -21,15 +26,14 @@ const Modal = ({ children }: Props) => {
     document.body.classList.add('modal-open');
     return () => {
       document.body.classList.remove('modal-open');
+      window.scrollTo(0, scrollY.current);
     };
   }, []);
   return (
-    <section
-      ref={ref}
-      className="fixed top-0 flex flex-col justify-center items-center max-w-md w-screen h-full z-50 bg-neutral-900/70 transition-transform"
-    >
+    <section className="fixed top-0 flex flex-col justify-center items-center max-w-md w-screen h-full z-50 bg-neutral-900/70 transition-transform">
       {/* <div className="">모달적용</div> */}
-      <div className="overflow-y-auto bg-white max-w-7xl w-full ">
+
+      <div className="overflow-y-auto max-w-7xl" ref={ref}>
         {children}
       </div>
     </section>
