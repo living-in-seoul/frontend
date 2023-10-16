a0d5fd2ac17d0261395019a1eda5d4e0045c47ec
최은석 <trgf456@naver.com>
2023-10-16 21:22:39 +0900

    [fix] provider

---
+++ src/app/providers.tsx
@@ -0,0 +1,20 @@
+import { NavigationEvents } from '@/components/map/bottomsheet/BottomSheetRouterEvent';
+import {
+  RecoilProvider,
+  SWRConfigContext,
+  SSEProvider,
+  ProgressBarProviders,
+} from '@/context';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <RecoilProvider>
+      <SWRConfigContext>
+        <SSEProvider>
+          <ProgressBarProviders>{children}</ProgressBarProviders>
+        </SSEProvider>
+      </SWRConfigContext>
+    </RecoilProvider>
+  );
+};
+export default Providers;
